8444d89a7552cd01f6b2bb5067f127357244d122
DaChickenGamer <rjmichalski2006@gmail.com>
2024-01-30 10:17:22 -0500

    Improved Timer
    I made it so it adds a 0 in front of the number if it is under ten so it looks better. I also made a text variable for each value.

--- main.ts
+++ main.ts
@@ -4,16 +4,38 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
+let secondsText = "";
+let minutesText = "";
+let hoursText = "";
+
 setInterval(TimerIncrement, 1000)
 
 function TimerIncrement()
 {
     seconds += 1;
 
-    if(seconds > 60)
+    if(seconds > 59)
+    {
+        seconds -= 60;
         minutes += 1;
-    if(minutes > 60)
+    }
+    if(minutes > 59)
+    {
+        minutes -= 60;
         hours += 1;
+    }
+    if(seconds < 10)
+        secondsText = "0" + seconds.toString();
+    else
+        secondsText = seconds.toString();
+    if(minutes < 10)
+        minutesText = "0" + minutes.toString();
+    else
+        minutesText = minutes.toString();
+    if(hours < 10)
+        hoursText = "0" + hours.toString();
+    else
+        hoursText = hours.toString();
 
-    timerText.innerText = hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
+    timerText.innerText = hoursText + ":" + minutesText + ":" + secondsText;
 }
