d7f9b2cc2a42a81544e3ae19584aaea49f4aca9d
DaChickenGamer <rjmichalski2006@gmail.com>
2024-01-30 11:42:35 -0500

    Print Data
    You can now print the data of the timers.

--- main.ts
+++ main.ts
@@ -10,49 +10,13 @@ let secondsText = "";
 let minutesText = "";
 let hoursText = "";
 
+let timeStorage = {};
+let currentRecord = 0;
+
 const timer = setInterval(TimerIncrement, 1000)
 
-function TimeToString(hours, minutes, seconds)
+function FormatTime(hours, minutes, seconds)
 {
-    return hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
-}
-function StartTimer()
-{
-    timerStarted = true;
-}
-function StopTimer()
-{
-    timerStarted = false;
-}
-function ClearTimer()
-{
-    seconds = 0;
-    minutes = 0;
-    hours = 0;
-
-    secondsText = "0" + seconds.toString();
-    minutesText = "0" + minutes.toString();
-    hoursText = "0" + hours.toString();
-
-    timerText.innerText = hoursText + ":" + minutesText + ":" + secondsText;
-}
-
-function TimerIncrement()
-{
-    if (timerStarted == false)
-        return;
-    seconds += 1;
-
-    if(seconds > 59)
-    {
-        seconds -= 60;
-        minutes += 1;
-    }
-    if(minutes > 59)
-    {
-        minutes -= 60;
-        hours += 1;
-    }
     if(seconds < 10)
         secondsText = "0" + seconds.toString();
     else
@@ -66,5 +30,65 @@ function TimerIncrement()
     else
         hoursText = hours.toString();
 
+    return hoursText + ":" + minutesText + ":" + secondsText;
+}
+function StartTimer()
+{
+    timerStarted = true;
+}
+function StopTimer()
+{
+    timerStarted = false;
+}
+function ClearTimer()
+{
+    timeStorage[currentRecord] = FormatTime(hours, minutes, seconds);
+    currentRecord += 1;
+
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    secondsText = "0" + seconds.toString();
+    minutesText = "0" + minutes.toString();
+    hoursText = "0" + hours.toString();
+
     timerText.innerText = hoursText + ":" + minutesText + ":" + secondsText;
+}
+function PrintData()
+{
+    if (document.getElementById("printedData"))
+    {
+        document.getElementById("printedData").remove();
+    }
+
+    const displayInfoBody = document.createElement("div")
+    displayInfoBody.setAttribute("id", "printedData")
+    document.body.appendChild(displayInfoBody);
+    for (let timeStorageKey in timeStorage) {
+        const displayInfoText = document.createElement("h3");
+        displayInfoText.innerText = "[" + timeStorageKey + "]" + ": " + timeStorage[timeStorageKey];
+        displayInfoBody.appendChild(displayInfoText);
+    }
+
+}
+
+function TimerIncrement()
+{
+    if (timerStarted == false)
+        return;
+    seconds += 1;
+
+    if (seconds > 59)
+    {
+        seconds -= 60;
+        minutes += 1;
+    }
+    if (minutes > 59)
+    {
+        minutes -= 60;
+        hours += 1;
+    }
+
+    timerText.innerText = FormatTime(hours, minutes, seconds);
 }
